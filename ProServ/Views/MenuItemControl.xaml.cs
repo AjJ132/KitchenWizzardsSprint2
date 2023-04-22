6b4f1a578980ae0c22238f12c28925cc1ccc249f
AjJ132 <54604582+AjJ132@users.noreply.github.com>
2023-04-22 17:23:49 -0400

    Menu Section

---
+++ ProServ/Views/MenuItemControl.xaml.cs
@@ -0,0 +1,36 @@
+﻿using ProServ.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProServ.Views
+{
+    /// <summary>
+    /// Interaction logic for MenuItemControl.xaml
+    /// </summary>
+    public partial class MenuItemControl : UserControl
+    {
+
+        public Item ThisItem { get; set; }
+
+        public MenuItemControl(Item item)
+        {
+            InitializeComponent();
+
+            ThisItem = item;
+
+            DataContext = this;
+        }
+    }
+}
